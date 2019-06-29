ab0fc312443adc04856314280484e49063111e61
beelarr <bryonl@me.com>
2019-06-29 18:41:48 -0500

    Added logging out and refactor to state to pass child props around.

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,47 +1,74 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { Container, Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 
 import Routes from "./Routes";
-import userHasAuthenticated from "containers/Home";
 import "./App.css";
 
-export default () => {
-  const childProps = {
-    isAuthenticated: this.state.isAuthenticated,
-    userHasAuthenticated: this.userHasAuthenticated
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    } catch (e) {
+      if (e !== "No current user") alert(e);
+    }
+    this.setState({ isAuthenticating: false });
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
   };
 
-  const handleLogout = event => {
-    userHasAuthenticated(false);
+  handleLogout = async () => {
+    await Auth.signOut();
+    this.userHasAuthenticated(false);
   };
 
-  return (
-    <Container className="App container">
-      <Navbar fluid collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-          <Link to="/">Scratch</Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav pullRight>
-            {isAuthenticated ? (
-              <NavItem onClick={handleLogout}>Logout</NavItem>
-            ) : (
-              <Fragment>
-                <LinkContainer to="/signup">
-                  <NavItem>Signup</NavItem>
-                </LinkContainer>
-                <LinkContainer to="/login">
-                  <NavItem>Login</NavItem>
-                </LinkContainer>
-              </Fragment>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Routes childProps={childProps} />
-    </Container>
-  );
-};
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
+    return (
+      !this.state.isAuthenticating && (
+        <Container className="App container">
+          <Navbar fluid collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="#home">
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav pullRight>
+                {this.state.isAuthenticated ? (
+                  <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                ) : (
+                  <Fragment>
+                    <LinkContainer to="/signup">
+                      <NavItem>Signup</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                      <NavItem>Login</NavItem>
+                    </LinkContainer>
+                  </Fragment>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Routes childProps={childProps} />
+        </Container>
+      )
+    );
+  }
+}
--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -1,56 +1,71 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import { Auth } from "aws-amplify";
 
 import "./Login.css";
 
-export default () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
 
-  const validateForm = () => {
-    return email.length > 0 && password.length > 0;
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
   };
 
-  const userHasAuthenticated = auth =>
-    setIsAuthenticated({ isAuthenticated: auth });
-
-  const handleSubmit = async event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
     try {
-      await Auth.signIn(email, password);
-      alert("Logged in");
+      await Auth.signIn(this.state.email, this.state.password);
+	    this.props.userHasAuthenticated(true);
     } catch (e) {
       alert(e.message);
     }
   };
 
-  return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <FormLabel>Email</FormLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <FormLabel>Password</FormLabel>
-          <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
